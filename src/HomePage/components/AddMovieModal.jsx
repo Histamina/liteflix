3876d99015ef79883d557896d96d43d82a0faa1a
Julieta Gomez <55216740+Histamina@users.noreply.github.com>
2021-11-06 16:06:07 -0300

    Font url modifications - responsive fixes

--- src/HomePage/components/AddMovieModal.jsx
+++ src/HomePage/components/AddMovieModal.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from 'react';
 import { Row, Col, Form, Button, Modal, Input, Upload, Progress } from 'antd';
 import helpers from '../../helpers';
+import Navbar from '../../components/Navbar';
 
 const { Dragger } = Upload;
 
@@ -94,10 +95,13 @@ const AddMovieModal = ({ isModalVisible, closeModal, setRefresh }) => {
             {
                !ready ?
                <Fragment>
-                  <Col lg={10}>
+                  <Col lg={24} xs={24} className="navbar-mobile-wrapper">
+                     <Navbar />
+                  </Col>
+                  <Col lg={10} xs={24} className="title-wrapper" >
                      <h2>Agregar película</h2>
                   </Col>
-                  <Col lg={24} className="form-wrapper">
+                  <Col lg={24} xs={24} className="form-wrapper">
                      <Form form={form} onFinish={onFinish} size="large">
                         <Row justify="center" className="add-file-wrapper">
                            <Form.Item
@@ -118,8 +122,11 @@ const AddMovieModal = ({ isModalVisible, closeModal, setRefresh }) => {
                                           <path d="M15.9946 11.7082L8.1588 4.58472C7.27098 3.77767 5.83169 3.77767 4.94408 4.58472C4.05612 5.39167 4.05612 6.70015 4.94408 7.50732L11.7755 13.7177L12.1771 13.3522L5.34575 7.14207C4.67987 6.53675 4.67987 5.55532 5.34575 4.95C6.01167 4.34459 7.09114 4.34459 7.75695 4.95L15.593 12.0735C16.7027 13.0823 16.7027 14.7181 15.593 15.7268C14.4831 16.7357 12.6839 16.7357 11.5742 15.7268L2.7338 7.69017C1.18031 6.27778 1.18031 3.9879 2.7338 2.57551C4.28755 1.16327 6.80644 1.16327 8.36004 2.57551L15.1911 8.78587L15.593 8.42071L8.7614 2.21035C6.98561 0.596226 4.10671 0.596614 2.33128 2.21113C0.555884 3.82551 0.556311 6.44269 2.33213 8.05669L11.1726 16.092C12.5041 17.3027 14.6631 17.3027 15.9946 16.092C17.3263 14.8814 17.3263 12.9188 15.9946 11.7082" stroke="white"/>
                                        </svg>
                                     </Col>
-                                    <Col>
+                                    <Col className="dragger-text-desktop">
                                        <p><strong>Agregá un archivo</strong> o arrastralo y soltalo aquí </p>
+                                    </Col>
+                                    <Col className="dragger-text-mobile">
+                                       <p>Agregá un archivo</p>
                                     </Col>
                                  </Row>
                               </Upload.Dragger>
@@ -190,7 +197,7 @@ const AddMovieModal = ({ isModalVisible, closeModal, setRefresh }) => {
                               }
                         </Row>
                         <Row justify="center" style={{ marginTop: 48 }}>
-                           <Col lg={10}>
+                           <Col lg={10} xs={18}>
                               <Form.Item
                                  name="original_title"
                                  rules={[{
@@ -204,7 +211,7 @@ const AddMovieModal = ({ isModalVisible, closeModal, setRefresh }) => {
                            </Col>
                         </Row>   
                         <Row justify="center">
-                           <Col lg={10}>
+                           <Col lg={10} xs={18}>
                               <Button
                                  htmlType="submit"
                                  type="primary"
@@ -232,7 +239,7 @@ const AddMovieModal = ({ isModalVisible, closeModal, setRefresh }) => {
                            <p><strong>¡Felicitaciones!</strong></p>
                         </Col>            
                         <Col lg={24}>
-                           <p className="movie-title">{movieTitle} la película fue correctamente subida.</p>
+                           <p className="movie-title">{movieTitle} fue correctamente subida.</p>
                         </Col>            
                         <Col lg={8}>
                            <Button
